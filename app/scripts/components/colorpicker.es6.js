8e4cc8342518457dbf05feb6394f9c8fa32f5e7c
Robin Böhm <robin.boehm@symetics.com>
2015-07-28 10:41:37 +0200

    add basic babel+browserify+gulp build

---
+++ app/scripts/components/colorpicker.es6.js
@@ -0,0 +1,30 @@
+function NavigatorNavbar () {
+  /**
+   * The view model for the Navigator Navbar
+   */
+  class NavigatorNavbarVM {
+    constructor ($routeSegment, APP_CONFIG) {
+      'ngInject';
+      this.$routeSegment = $routeSegment;
+      this.apps = Object.keys(APP_CONFIG.apps).filter((key) => APP_CONFIG.apps[key]);
+    }
+  }
+
+
+  // return the directive definition object
+  return {
+    restrict: 'E',
+    scope: {},
+    templateUrl: 'navigator/navigator-navbar.html',
+    controller: NavigatorNavbarVM,
+    controllerAs: 'vm',
+    bindToController: {
+      title: '='
+    }
+  };
+}
+
+angular.module('myApp')
+  .directive('navigatorNavbar', NavigatorNavbar);
+
+export default NavigatorNavbar;

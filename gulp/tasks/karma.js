42687ad3c0a762bdfab8429938912249742148ba
Robin Böhm <robin.boehm@symetics.com>
2015-07-29 16:51:18 +0200

    add karma task to gulp

--- gulp/tasks/karma.js
+++ gulp/tasks/karma.js
@@ -1,3 +1,13 @@
-/**
- * Created by robinboehm on 29/07/15.
- */
+var gulp = require('gulp');
+var karma = require('karma');
+
+var karmaTask = function(done) {
+  karma.server.start({
+    configFile: process.cwd() + '/karma.conf.js',
+    singleRun: true
+  }, done);
+};
+
+gulp.task('karma', karmaTask);
+
+module.exports = karmaTask;

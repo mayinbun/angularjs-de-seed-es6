cf4bc79d6908d2e4d5fc344a405374300f86022d
Robin Böhm <robin.boehm@symetics.com>
2015-08-01 19:40:51 +0200

    chore(cleanup): remove test directive

--- app/components/navigator/navigator.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import navigationNavbarDirective from './navigator-navbar.directive'
-import bookData from '../bookData'
-
-angular.module('navigator', [bookData])
-  .directive('navigatorNavbar', navigationNavbarDirective);
-
-export default 'navigator'

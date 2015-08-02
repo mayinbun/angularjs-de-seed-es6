ba765c2eeab7c919d88974eac8c9b33afdd73b14
Robin Böhm <robin.boehm@symetics.com>
2015-08-02 17:34:20 +0200

    feat(bookData): remove feature

--- app/components/bookData/book-data.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-
-import BookDataService from './book-data.service'
-
-angular.module('bookDataModule', [])
-  .service('bookData', BookDataService);
-
-export default 'bookDataModule'

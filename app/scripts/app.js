b272c74ac2a9c5b587e7f62b0a6f267d9f79fe05
Robin Böhm <robin.boehm@symetics.com>
2015-08-05 11:31:50 +0200

    add angular aria

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,26 @@
 import angular from 'angular';
 import angularRoute from 'angular-route'
+import angularAria from 'angular-aria'
 
+// Modules
 import BookDataModule from '../components/book-data'
 import BookListModule from '../components/book-list'
 import BookDetailModule from '../components/book-detail'
+import BookFormModule from '../components/book-form'
+
+// Configs
 import RouteConfig from './configs/route.config'
 
 angular.module('myApp', [
 
   // 3rd party || angular libs
   angularRoute,
+  angularAria,
 
   // my libs
   BookDataModule,
   BookListModule,
-  BookDetailModule
+  BookDetailModule,
+  BookFormModule
 ])
 .config(RouteConfig);
